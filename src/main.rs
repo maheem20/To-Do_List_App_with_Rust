abf027c5f8071edc5adf58bdfba15c2cfe870715
maheem_20 <mwistanbuly@gmail.com>
2023-06-07 22:28:56 +0200

    add new method to fix overwriting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashMap, str};
+use std::io::Read;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
@@ -29,6 +30,23 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         // insert a new item into our map
         // we pass true as value
